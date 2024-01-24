58255150c6fc83da8a40517fe6ae3ade7ce88eca
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2024-01-25 00:34:00 +0100

    multiple

--- get-last-entry.js
+++ get-last-entry.js
@@ -1,22 +1,45 @@
-
-const dotenv = require('dotenv');
-
-dotenv.config();
-const userId = process.env.ZOTERO_USER_ID;
-const apiKey = process.env.ZOTERO_API_KEY;
-
-// config
-const limit = 2;
-const url = `https://api.zotero.org/users/${userId}/items/?format=bibtex&limit=${limit}&key=${apiKey}`;
-
 async function main() {
-    const response = await fetch(url);
-    let bibtex = await response.text();
+
+    const dotenv = require('dotenv');
+
+    dotenv.config();
+    const userId = process.env.ZOTERO_USER_ID;
+    const apiKey = process.env.ZOTERO_API_KEY;
+
+    // config
+    const limit = 2;
+    let urls = [`https://api.zotero.org/users/${userId}/items/?format=bibtex&limit=${limit}&key=${apiKey}`];
+
+    // new: get url from clipboard (node!)
+    const clipboardy = (await import('clipboardy')).default;
+    const zoteroFrontendUrl = clipboardy.readSync(); // https://www.zotero.org/johannesbrandenburger/items/YIKFHIES,9N898HM3/item-details
+
+    if (zoteroFrontendUrl.includes("https://www.zotero.org")) {
+
+        // extract item keys
+        const itemKeys = zoteroFrontendUrl.split('/items/')[1].split('/item-details')[0].split(',');
+        console.log(itemKeys);
+
+        // build urls
+        urls = itemKeys.map(itemKey => `https://api.zotero.org/users/${userId}/items/${itemKey}?format=bibtex&key=${apiKey}`);
+    }
+    
+    
+    console.log(urls);
+    let bibtex = "";
+    for (let i = 0; i < urls.length; i++) {
+        const url = urls[i];
+        const response = await fetch(url);
+        const text = await response.text();
+        bibtex += text + "\n";
+    }
 
     // replace newlines with spaces to have it in one line and trim
-    bibtex = bibtex.replace(/\n/g, ' ');
     bibtex = bibtex.trim();
-    
+
+    // copy to clipboard
+    clipboardy.writeSync(bibtex);
+
     console.log(bibtex);
 }
 
